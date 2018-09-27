cd4fb51dc126880f115491dbff70d5b34d731376
Edionay Aguiar <edionay@gmail.com>
2018-09-27 18:01:57 -0300

    Bid  Class was created.

---
+++ src/main/java/Bid.java
@@ -0,0 +1,20 @@
+import javax.jws.soap.SOAPBinding;
+
+public class Bid {
+
+    private User user;
+    private double value;
+
+    public Bid(User user, double value) {
+        this.user = user;
+        this.value = value;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
